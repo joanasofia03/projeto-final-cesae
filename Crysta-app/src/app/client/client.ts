d0936c67d17cb5c57ea3f47cee242da5859968e1
joanaperpetuo263162630 <joana.perpetuo.263162630@msft.cesae.pt>
2025-07-14 22:34:07 +0100

    Consulta das transações do Cliente

--- Crysta-app/src/app/client/client.ts
+++ Crysta-app/src/app/client/client.ts
@@ -12,14 +12,33 @@ import { HttpClientModule } from '@angular/common/http';
 })
 export class ClientComponent implements OnInit {
   balances: any[] = [];
+  transactions: any[] = [];
 
   constructor(private http: HttpClient) {}
 
   ngOnInit(): void {
+    console.log('ngOnInit called');
+    this.loadBalances();
+    this.loadTransactions();
+  }
+
+  loadBalances() {
     this.http.get<any[]>('http://localhost:5146/api/dim_account/my-balance')
       .subscribe({
         next: data => this.balances = data,
         error: err => console.error('Failed to load balances:', err)
+      });
+  }
+
+  loadTransactions() {
+    console.log('loadTransactions called');
+    this.http.get<any[]>('http://localhost:5146/api/fact_transactions/getmine')
+      .subscribe({
+        next: data => {
+          console.log('Transactions loaded:', data);
+          this.transactions = data;
+        },
+        error: err => console.error('Failed to load transactions:', err)
       });
   }
 }
