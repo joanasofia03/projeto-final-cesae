6bf1a43a09bc463324ed43f5f7688cc4e4a2b980
joanaperpetuo263162630 <joana.perpetuo.263162630@msft.cesae.pt>
2025-07-15 22:28:33 +0100

    Cliente pode ver a evolução do seu saldo, o seu saldo total e a média de gastos

--- Crysta-app/src/app/client/client.ts
+++ Crysta-app/src/app/client/client.ts
@@ -21,29 +21,34 @@ export class ClientComponent implements OnInit {
   transactions: any[] = [];
   notifications: any[] = [];
   deposit = {
-  accountId: null,
-  amount: null,
-  channel: 'Web',
-  status: 'Processed',
+    accountId: null,
+    amount: null,
+    channel: 'Web',
+    status: 'Processed',
   };
   transaction = {
-  sourceAccountId: null,
-  destinationAccountId: null,
-  amount: null,
-  channel: 'Web',
-  status: 'Processed'
+    sourceAccountId: null,
+    destinationAccountId: null,
+    amount: null,
+    channel: 'Web',
+    status: 'Processed'
   };
   filters = {
-  from: '',
-  to: '',
-  type: null
+    from: '',
+    to: '',
+    type: null
   };
   transactionTypes = [
-  { id: 1, label: 'Transfer' },
-  { id: 2, label: 'Deposit' }
+    { id: 1, label: 'Transfer' },
+    { id: 2, label: 'Deposit' }
   ];
+  balanceEvolution: { date: string; balance: number }[] = [];
   transactionError: string | null = null;
-
+  totalBalance: number | null = null;
+  totalBalanceError: string | null = null;
+  averageSpending: number | null = null;
+  monthlyBreakdown: any[] = [];
+  averageSpendingError: string | null = null;
 
   constructor(private http: HttpClient) { }
 
@@ -51,7 +56,18 @@ export class ClientComponent implements OnInit {
     console.log('ngOnInit called');
     this.loadBalances();
     this.loadTransactions();
+    this.http.get<any[]>('http://localhost:5146/api/analytics/balance-evolution')
+    .subscribe({
+      next: (data) => {
+        this.balanceEvolution = data;
+      },
+      error: (err) => {
+        console.error('Failed to load balance evolution:', err);
+      }
+    });
     this.loadNotifications();
+    this.loadTotalBalance();
+    this.loadAverageSpending();
   }
 
   loadBalances() {
@@ -85,142 +101,180 @@ export class ClientComponent implements OnInit {
       });
   }
 
-  makeDeposit() {
-  if (!this.deposit.accountId || !this.deposit.amount || this.deposit.amount <= 0) {
-    alert("Please select a valid account and enter a positive amount.");
-    return;
-  }
+  loadTotalBalance() {
+    console.log('Fetching total balance...');
 
-  const token = localStorage.getItem('auth_token');
-  if (!token) {
-    alert("User is not authenticated.");
-    return;
-  }
-
-  const decoded = jwtDecode<JwtPayload>(token);
-  const userId = parseInt(decoded.nameid, 10);
-  if (isNaN(userId)) {
-    alert("Invalid user ID in token.");
-    return;
-  }
-
-  const depositDto = {
-    AppUser_ID: userId,
-    Source_Account_ID: this.deposit.accountId,
-    Destination_Account_ID: this.deposit.accountId,
-    Transaction_Amount: this.deposit.amount,
-    Execution_Channel: this.deposit.channel,
-    Transaction_Status: this.deposit.status
-  };
-
-  this.http.post('http://localhost:5146/api/fact_transactions/deposit', depositDto)
-    .subscribe({
-      next: (res) => {
-        alert('Deposit successful!');
-        console.log('Deposit:', depositDto);
-        this.loadBalances();
-        this.loadTransactions();
-      },
-      error: (err) => {
-        console.error('Deposit failed', err);
-        alert('Deposit failed: ' + (err.error?.Message || 'Unknown error'));
-      }
-    });
-
-    
-}
-
-makeTransaction() {
-  if (
-    !this.transaction.sourceAccountId ||
-    !this.transaction.destinationAccountId ||
-    !this.transaction.amount ||
-    this.transaction.amount <= 0
-  ) {
-    alert("Please fill all fields and enter a valid amount.");
-    return;
-  }
-
-  if (this.transaction.sourceAccountId === this.transaction.destinationAccountId) {
-    alert("Source and destination accounts must be different.");
-    return;
-  }
-
-  const token = localStorage.getItem('auth_token');
-  if (!token) {
-    alert("User is not authenticated.");
-    return;
-  }
-
-  const decoded = jwtDecode<JwtPayload>(token);
-  const userId = parseInt(decoded.nameid, 10);
-  if (isNaN(userId)) {
-    alert("Invalid user ID in token.");
-    return;
-  }
-
-  const transferDto = {
-    AppUser_ID: userId,
-    Source_Account_ID: this.transaction.sourceAccountId,
-    Destination_Account_ID: this.transaction.destinationAccountId,
-    Transaction_Amount: this.transaction.amount,
-    Execution_Channel: this.transaction.channel,
-    Transaction_Status: this.transaction.status,
-    Transaction_Type_ID: 2
-  };
-
-  this.http.post('http://localhost:5146/api/fact_transactions/create-transaction', transferDto)
-    .subscribe({
-      next: (res) => {
-        alert('Transaction successful!');
-        this.loadBalances();
-        this.loadTransactions();
-      },
-      error: (err) => {
-        console.error('Deposit failed', err);
-
-        const backendError = err.error?.error || 'Unknown Error';
-        const backendMessage = err.error?.message || 'An unknown error occurred.';
-
-        alert(`Deposit failed: ${backendError} - ${backendMessage}`);
-      }
-    });
-
-    
-  }
-  
-  filterTransactions() {
-  const params: any = {};
-
-  if (this.filters.from) params.from = this.filters.from;
-  if (this.filters.to) params.to = this.filters.to;
-  if (this.filters.type) params.type = this.filters.type;
-
-  this.http.get<any[]>('http://localhost:5146/api/fact_transactions/filter', { params })
-    .subscribe({
-      next: (data) => {
-        this.transactions = data;
-        this.transactionError = null; // clear old error if any
-      },
-      error: (err) => {
-        if ( err.error?.message) {
-          this.transactionError = err.error.message;
-          alert(`${this.transactionError}`);
-        } else {
-          this.transactionError = 'An unexpected error occurred.';
-          alert('An unexpected error occurred while filtering transactions.');
+    this.http.get<{ totalBalance: number }>('http://localhost:5146/api/analytics/total-balance')
+      .subscribe({
+        next: data => {
+          console.log('Total balance response:', data);
+          this.totalBalance = data.totalBalance;
+          this.totalBalanceError = null;
+        },
+        error: err => {
+          console.error('Error fetching total balance:', err);
+          this.totalBalance = null;
+          this.totalBalanceError = err.error?.message || 'Failed to fetch total balance.';
         }
-        this.transactions = [];
+      });
+  }
+
+  loadAverageSpending(): void {
+    console.log('Loading average spending...');
+    this.http.get<{ averageSpending: number, monthlyBreakdown: any[] }>(
+      'http://localhost:5146/api/analytics/average-spending'
+    ).subscribe({
+      next: data => {
+        console.log('Average spending response:', data);
+        this.averageSpending = data.averageSpending;
+        this.monthlyBreakdown = data.monthlyBreakdown;
+        this.averageSpendingError = null;
+      },
+      error: err => {
+        console.error('Error loading average spending:', err);
+        this.averageSpending = null;
+        this.monthlyBreakdown = [];
+        this.averageSpendingError = err.error?.message || 'Failed to fetch average spending.';
       }
     });
-}
+  }
 
+  makeDeposit() {
+    if (!this.deposit.accountId || !this.deposit.amount || this.deposit.amount <= 0) {
+      alert("Please select a valid account and enter a positive amount.");
+      return;
+    }
 
-resetFilters() {
-  this.filters = { from: '', to: '', type: null };
-  this.loadTransactions();
-}
+    const token = localStorage.getItem('auth_token');
+    if (!token) {
+      alert("User is not authenticated.");
+      return;
+    }
+
+    const decoded = jwtDecode<JwtPayload>(token);
+    const userId = parseInt(decoded.nameid, 10);
+    if (isNaN(userId)) {
+      alert("Invalid user ID in token.");
+      return;
+    }
+
+    const depositDto = {
+      AppUser_ID: userId,
+      Source_Account_ID: this.deposit.accountId,
+      Destination_Account_ID: this.deposit.accountId,
+      Transaction_Amount: this.deposit.amount,
+      Execution_Channel: this.deposit.channel,
+      Transaction_Status: this.deposit.status
+    };
+
+    this.http.post('http://localhost:5146/api/fact_transactions/deposit', depositDto)
+      .subscribe({
+        next: (res) => {
+          alert('Deposit successful!');
+          console.log('Deposit:', depositDto);
+          this.loadBalances();
+          this.loadTransactions();
+        },
+        error: (err) => {
+          console.error('Deposit failed', err);
+          alert('Deposit failed: ' + (err.error?.Message || 'Unknown error'));
+        }
+      });
+
 
   }
 
-  
+  makeTransaction() {
+    if (
+      !this.transaction.sourceAccountId ||
+      !this.transaction.destinationAccountId ||
+      !this.transaction.amount ||
+      this.transaction.amount <= 0
+    ) {
+      alert("Please fill all fields and enter a valid amount.");
+      return;
+    }
+
+    if (this.transaction.sourceAccountId === this.transaction.destinationAccountId) {
+      alert("Source and destination accounts must be different.");
+      return;
+    }
+
+    const token = localStorage.getItem('auth_token');
+    if (!token) {
+      alert("User is not authenticated.");
+      return;
+    }
+
+    const decoded = jwtDecode<JwtPayload>(token);
+    const userId = parseInt(decoded.nameid, 10);
+    if (isNaN(userId)) {
+      alert("Invalid user ID in token.");
+      return;
+    }
+
+    const transferDto = {
+      AppUser_ID: userId,
+      Source_Account_ID: this.transaction.sourceAccountId,
+      Destination_Account_ID: this.transaction.destinationAccountId,
+      Transaction_Amount: this.transaction.amount,
+      Execution_Channel: this.transaction.channel,
+      Transaction_Status: this.transaction.status,
+      Transaction_Type_ID: 2
+    };
+
+    this.http.post('http://localhost:5146/api/fact_transactions/create-transaction', transferDto)
+      .subscribe({
+        next: (res) => {
+          alert('Transaction successful!');
+          this.loadBalances();
+          this.loadTransactions();
+        },
+        error: (err) => {
+          console.error('Deposit failed', err);
+
+          const backendError = err.error?.error || 'Unknown Error';
+          const backendMessage = err.error?.message || 'An unknown error occurred.';
+
+          alert(`Deposit failed: ${backendError} - ${backendMessage}`);
+        }
+      });
+
+
+  }
+
+  filterTransactions() {
+    const params: any = {};
+
+    if (this.filters.from) params.from = this.filters.from;
+    if (this.filters.to) params.to = this.filters.to;
+    if (this.filters.type) params.type = this.filters.type;
+
+    this.http.get<any[]>('http://localhost:5146/api/fact_transactions/filter', { params })
+      .subscribe({
+        next: (data) => {
+          this.transactions = data;
+          this.transactionError = null; // clear old error if any
+        },
+        error: (err) => {
+          if (err.error?.message) {
+            this.transactionError = err.error.message;
+            alert(`${this.transactionError}`);
+          } else {
+            this.transactionError = 'An unexpected error occurred.';
+            alert('An unexpected error occurred while filtering transactions.');
+          }
+          this.transactions = [];
+        }
+      });
+  }
+
+
+  resetFilters() {
+    this.filters = { from: '', to: '', type: null };
+    this.loadTransactions();
+  }
+
+}
+
+
